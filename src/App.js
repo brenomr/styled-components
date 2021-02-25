f9520a8400ab16a6e324e89133febeab1fab4782
Breno Rodrigues <brenocartoon@hotmail.com>
2021-02-25 11:55:47 -0300

    Finalizacao do curso

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Cabecalho from "./Components/Cabecalho";
 import Container from "./Components/Container";
 import { GlobalStyle } from './Components/globalStyle'
+import { ThemeProvider } from 'styled-components';
+import { temaEscuro, temaClaro } from './Components/UI/temas';
+import { BtnTema } from "./Components/UI";
+import SwitchTema from "./Components/SwitchTema";
 
 function App() {
+  const [tema, setTema] = useState(true);
+
+  const toggleTema = () => {
+    setTema((tema) => !tema);
+  }
   return (
-    <>
-      <GlobalStyle />
-      <Cabecalho />
-      <Container />
-    </>
+      <ThemeProvider theme={tema ? temaClaro : temaEscuro}>
+        <GlobalStyle />
+        <BtnTema onClick={toggleTema}>
+          <SwitchTema tema={tema} />
+        </BtnTema>
+        <Cabecalho />
+        <Container />
+      </ThemeProvider>
   );
 }
 
